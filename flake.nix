3d1043a2a117566acf7717911f8f8fd5675a80af
Joshua Perry <josh@6bit.com>
2024-02-29 18:40:29 -0700

    Add the flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Kubebuilder Flake";
+
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-23.11"; };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, devenv }:
+  flake-parts.lib.mkFlake { inherit inputs; } {
+    imports = [ inputs.devenv.flakeModule ];
+    systems = nixpkgs.lib.systems.flakeExposed;
+
+    perSystem = { config, system, pkgs, ... }: {
+
+      devenv.shells.default = {
+        languages.go.enable = true;
+
+        env = {
+          GOPROXY="https://goproxy.io,direct";
+        };
+
+        packages = with pkgs; [
+          gopls
+          gotools
+          go-tools
+          kubebuilder
+          kustomize
+          gnumake
+        ];
+      };
+
+    };
+
+  };
+}
